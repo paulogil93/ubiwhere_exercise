e43c2824b15579936f93fbc8bf518f711602db40
Paulo Gil <paulogil93@gmail.com>
2020-07-04 15:45:11 +0100

    Created a blank Django Project. Edited setting.py to work with rest_framework and PostGIS database. Added Incident Model.

---
+++ exercise/api/models.py
@@ -0,0 +1,32 @@
+from django.contrib.auth.models import User
+from django.contrib.gis.db import models
+
+class Incident(models.Model):
+    # Não há necessidade de definir id = models.AutoField(primary_key=True)
+    # O Django trata deste field automaticamente por nós :)
+    description = models.TextField()
+    location = models.PointField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE) # FIXME: Referenciar username ou id? Check Paulo, check.
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    STATUS_CHOICES = [
+        ('VALIDATE','Validate'),    # FIXME: VALIDATE e VALIDATED parecem ambíguos e podem induzir em erro?
+        ('VALIDATED','Validated'),
+        ('RESOLVED', 'Resolved')
+    ]
+
+    status = models.CharField(choices=STATUS_CHOICES, default='VALIDATE')
+
+    CATEGORY_CHOICES = [
+        ('CONSTRUCTION', 'Construction'),
+        ('SPECIAL_EVENT', 'Special event'),
+        ('INCIDENT', 'Incident'),
+        ('WEATHER_CONDITION', 'Weather condition'),
+        ('ROAD_CONDITION', 'Road condition')
+    ]
+
+    category = models.CharField(choices=CATEGORY_CHOICES)
+
+    class Meta:
+        ordering = ['created']
