0f3548a7bc4ab0380c42873fd400023813e0b327
Paulo Gil <paulogil93@gmail.com>
2020-07-09 11:47:37 +0100

    Added an extra action to enable filtering Incidents by location

--- exercise/api/views.py
+++ exercise/api/views.py
@@ -4,6 +4,10 @@ from rest_framework import viewsets
 from api.serializers import UserSerializer, GroupSerializer, IncidentSerializer
 from django_filters import rest_framework as filters
 from api.filters import UserFilter, IncidentFilter
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from django.contrib.gis.geos import Point
+from django.contrib.gis.measure import Distance
 
 class UserViewSet(viewsets.ModelViewSet):
     """
@@ -34,3 +38,36 @@ class IncidentViewSet(viewsets.ModelViewSet):
     # do tipo Incident com o utilizador atual
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
+
+    @action(detail=False, methods=['GET'], name='Filter by location')
+    def filterByLocation(self, request):
+        latitude = request.GET.get('latitude')
+        longitude = request.GET.get('longitude')
+        radius = request.GET.get('radius')
+
+        # Asserts necessários para que a extra action não rebente :)
+        if radius is None or not radius.isnumeric():
+            return Response({'RadiusError': 'Either Radius is None or it\'s not Numeric.'})
+        if latitude is None or not latitude.isnumeric():
+            return Response({'LatitudeError': 'Either Latitude is None or it\'s not Numeric.'})
+        if longitude is None or not longitude.isnumeric():
+            return Response({'LongitudeError': 'Either Longitude is None or it\'s not Numeric.'})
+
+        # Ponto de partida, centro do raio de localização
+        origin = Point(float(longitude), float(latitude))
+        # Distância em quilómetros do raio de localização
+        distance = Distance(km=float(radius))
+
+        # Filtra e devolve todos os objectos do tipo Incident que estejam a uma distância menor que o raio de pesquisa
+        queryset = Incident.objects.filter(location__distance_lte=(origin, distance))
+	# Cria uma página com essa queryset
+        page = self.paginate_queryset(queryset)
+
+	# Se o resultado não for vazio, serializa o queryset e devolve esse resultado paginado
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+
+        serializer = self.get_serializer(queryset, many=True)
+        
+        return Response(serializer.data)
